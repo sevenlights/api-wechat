b88db7c0a33ed5084c5d2f52f9c069bf67754730
zhangweihong <zhangwh2020@sina.com>
2018-10-02 16:23:09 +0800

    up

--- src/main/java/com/sevenlight/controller/AccountController.java
+++ src/main/java/com/sevenlight/controller/AccountController.java
@@ -2,7 +2,9 @@ package com.sevenlight.controller;
 
 import com.sevenlight.entity.UserInfo;
 import com.sevenlight.entity.WxLoginInfo;
+import com.sevenlight.util.HttpUtil;
 import com.sevenlight.util.ResponseUtil;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("wx/auth")
 public class AccountController {
+    //private $authApi = 'https://open.weixin.qq.com/connect/oauth2/authorize';
+    //private $authAccessTokenApi = 'https://api.weixin.qq.com/sns/oauth2/access_token';
+    //private $authAccessTokenApi4XCX = 'https://api.weixin.qq.com/sns/jscode2session';
+    //private $userInfoApi = 'https://api.weixin.qq.com/sns/userinfo';
+    //private $jsTicketApi = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket';
+    //private $accessTokenApi = 'https://api.weixin.qq.com/cgi-bin/token';
+    //private $unifiedApi = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
+
+    /*$url = $this->authAccessTokenApi;
+    $querys = array(
+      'appid'  => $this->appId,
+            'secret' => $this->appSecret,
+            'grant_type' => 'authorization_code'
+            );
+    if ($this->platform && $this->platform == Platform::WX_XCX) {
+        $querys['js_code'] = $code;
+        $url = $this->authAccessTokenApi4XCX;
+    } else {
+        $querys['code'] = $code;
+    }*/
+
+
+
     private static final Logger log = LoggerFactory.getLogger(AccountController.class);
     @RequestMapping("/wxlogin")
     public Object wxLogin(@RequestBody WxLoginInfo wxLoginInfo, HttpServletRequest request) {
@@ -28,6 +56,11 @@ public class AccountController {
         String sessionKey = null;
         String openId = null;
         //调用微信接口获取openid和sessionKey
+        String url = "https://api.weixin.qq.com/sns/jscode2session";
+        Map<String, String> params = new HashMap<>();
+        params.put("js_code", code);
+        Response res = HttpUtil.okHttpGet(url, params);
+        log.info("result is {}",res);
         //通过openId和sessionKey获取用户信息或判断用户是否已注册
 
 
--- src/main/java/com/sevenlight/util/HttpUtil.java
+++ src/main/java/com/sevenlight/util/HttpUtil.java
@@ -1,11 +1,16 @@
 package com.sevenlight.util;
 
+import okhttp3.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -69,4 +74,73 @@ public class HttpUtil {
         return result.toString();
     }
 
+    public static Response okHttpGet(String url, Map<String, String> params) {
+        url = appendUri(url, params);
+        OkHttpClient httpClient = new OkHttpClient();
+        Request.Builder builder = new Request.Builder();
+        Request request1 = builder.get().url(url).build();
+        Call call = httpClient.newCall(request1);
+        Response response = null;
+        try{
+            response = call.execute();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public static Response okHttpPost(String url, Map<String, String> bodyParams) {
+        OkHttpClient httpClient = new OkHttpClient();
+        RequestBody body = setRequestBody(bodyParams);
+        Request.Builder requestBuilder = new Request.Builder();
+        Request request = requestBuilder.post(body).url(url).build();
+        Call call = httpClient.newCall(request);
+        Response response = null;
+        try {
+            response = call.execute();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private static RequestBody setRequestBody(Map<String, String> bodyParams) {
+        RequestBody body = null;
+        FormBody.Builder formBodyBuilder = new FormBody.Builder();
+        if(bodyParams != null){
+            Iterator<String> iterator = bodyParams.keySet().iterator();
+            String key = "";
+            while (iterator.hasNext()) {
+                key = iterator.next().toString();
+                formBodyBuilder.add(key, bodyParams.get(key));
+            }
+        }
+        body = formBodyBuilder.build();
+        return body;
+    }
+
+    public static String appendUri(String uri, Map<String, String> params) {
+        URI newUri = null;
+        try {
+            URI oldUri = new URI(uri);
+            String newQuery = oldUri.getQuery();
+
+            if (params != null) {
+                Iterator<String> iterator = params.keySet().iterator();
+                while (iterator.hasNext()) {
+                    String key = iterator.next().toString();
+                    if (newQuery == null) {
+                        newQuery = key + "=" + params.get(key);
+                    } else {
+                        newQuery += "&" + key + "=" + params.get(key);
+                    }
+                }
+            }
+            newUri = new URI(oldUri.getScheme(), oldUri.getAuthority(),
+                    oldUri.getPath(), newQuery, oldUri.getFragment());
+        }catch (URISyntaxException exp){
+            exp.printStackTrace();
+        }
+        return newUri.toString();
+    }
 }
